d8c52df45a92a40511d1f6761fd375a362b94317
wentao <wentao@sobey.com>
2021-06-11 15:12:38 +0800

    uuid

--- react-demo/src/index.js
+++ react-demo/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 // import  matchPath from './react-router/matchPath'
 import {createBrowserHistory} from './react-router/history/createBrowserHistory'
+import { v4 as uuidv4 } from 'uuid';
 
 ReactDOM.render(
   <React.StrictMode>
